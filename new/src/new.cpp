c97aed0a1f145c5629a5251c355ce1b6b7374bc7
eclpsr <balupwns@gmail.com>
2020-03-12 20:56:06 +0900

    SFML 4 - Keyboard Events

--- new/src/new.cpp
+++ new/src/new.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	sf::RenderWindow window(sf::VideoMode(200, 200), "Lesson 2. kychka-pc.ru");
+	sf::RenderWindow window(sf::VideoMode(600, 600), "eclpsr");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Red);
 
@@ -14,8 +13,33 @@ int main() {
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			switch ( event.type )
+			{
+			case sf::Event::Closed:
+				window.close( );
+				break;
+
+			case sf::Event::KeyPressed:
+				std:: cout << "Key pressed" << std::endl;
+
+				switch (event.key.code)
+					{
+					case sf::Keyboard::A:
+						std::cout << "A" << std::endl;
+						break;
+					}
+				break;
+				case sf::Event::KeyReleased:
+					std::cout << "Key has been released" << std::endl;
+					switch (event.key.code)
+					{
+					case sf::Keyboard::J:
+						std::cout << "J RELEASED" << std::endl;
+						break;
+					}
+				break;
+
+			}
 		}
 
 		window.clear();
