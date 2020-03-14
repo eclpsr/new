3e8ec472f4e8a177f05b4442ce1b2dc093462c7a
eclpsr <balupwns@gmail.com>
2020-03-14 23:16:36 +0900

    SFML 5 - Mouse Events

--- new/src/new.cpp
+++ new/src/new.cpp
@@ -5,8 +5,6 @@ using namespace std;
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(600, 600), "eclpsr");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Red);
 
 	while (window.isOpen())
 	{
@@ -16,34 +14,68 @@ int main() {
 			switch ( event.type )
 			{
 			case sf::Event::Closed:
-				window.close( );
-				break;
+					window.close( );
 
-			case sf::Event::KeyPressed:
-				std:: cout << "Key pressed" << std::endl;
+					break;
+
+			    case sf::Event::MouseButtonPressed:
+				std::cout << "Mouse button has been pressed" << std::endl;
+
+
+
+
 
 				switch (event.key.code)
-					{
-					case sf::Keyboard::A:
-						std::cout << "A" << std::endl;
-						break;
-					}
-				break;
-				case sf::Event::KeyReleased:
-					std::cout << "Key has been released" << std::endl;
-					switch (event.key.code)
-					{
-					case sf::Keyboard::J:
-						std::cout << "J RELEASED" << std::endl;
-						break;
-					}
+				{
+				case sf::Mouse::Left:
+					std::cout << "LEFT KEY HAS BEEN PRESSED" << std::endl;
+
+					break;
+				}
+
+				switch (event.key.code)
+				{
+				case sf::Mouse::Right:
+					std::cout << "Right key has been released" << std::endl;
+
+					break;
+				}
 				break;
 
+
+				case sf::Event::MouseButtonReleased:
+					std::cout << "Mouse button has been released" << std::endl;
+
+					break;
+
+
+
+					case sf::Event::MouseWheelMoved:
+						std::cout << "Mouse wheel has been scrolled" << std::endl;
+						std::cout << event.mouseWheel.delta << std::endl;
+
+						break;
+
+					case sf::Event::MouseMoved:
+						//std::cout << "Mouse has been moved" << std::endl;
+
+						//std::cout << event.mouseMove.y << std::endl;
+
+						break;
+
+					case sf::Event::MouseEntered:
+						std::cout << "Mouse has entered the window" << std::endl;
+
+						break;
+
+					case sf::Event::MouseLeft:
+						std::cout << "Mouse has left the window" << std::endl;
+
+						break;
 			}
 		}
 
 		window.clear();
-		window.draw(shape);
 		window.display();
 	}
 }
