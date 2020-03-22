c39981e2284ed214596bc77c636f3d993c0a5db6
eclpsr <balupwns@gmail.com>
2020-03-22 22:31:12 +0900

    SFML 29 - Drawing Using Vertex Arrays

--- new/src/new.cpp
+++ new/src/new.cpp
@@ -35,6 +35,42 @@ int main() {
 	circle.setTexture(&circletexture);
 	circle.setTextureRect(sf::IntRect(0, 0, 1500, 1500));
 
+	sf::Vertex point;
+	point.position = sf::Vector2f(300, 300);
+	point.color = sf::Color::Red;
+	sf::VertexArray line(sf::Lines, 2);
+
+	line[0].position = sf::Vector2f(200, 150);
+	line[0].color = sf::Color::Red;
+
+//	line[1].position = sf::Vector2f(400, 90);0
+//	line[1].color = sf::Color::Green;
+
+	line[1] = point;
+
+	sf::VertexArray triangle(sf::Triangles, 3);
+	triangle[0].position = sf::Vector2f(20, 50);
+	triangle[0].color = sf::Color::White;
+
+	triangle[1].position = sf::Vector2f(70, 250);
+	triangle[1].color = sf::Color::Red;
+
+	triangle[2].position = sf::Vector2f(10, 250);
+	triangle[2].color = sf::Color::Blue;
+
+	sf::VertexArray quad(sf::Quads, 4);
+
+	quad[0].position = sf::Vector2f(40, 50);
+	quad[0].color = sf::Color::Yellow;
+
+	quad[1].position = sf::Vector2f(80, 50);
+	quad[1].color = sf::Color::Red;
+
+	quad[2].position = sf::Vector2f(90, 200);
+	quad[2].color = sf::Color::Green;
+
+	quad[3].position = sf::Vector2f(10, 150);
+	quad[3].color = sf::Color::Cyan;
 
 
 	sf::Texture texture;
@@ -104,7 +140,13 @@ int main() {
 
 		window.clear();
 
+
+
 		window.draw(circle);
+
+		window.draw(triangle);
+
+		window.draw(quad);
 
 		window.draw(sprite);
 
